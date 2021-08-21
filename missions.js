1dd926ad6c648bdc4c9bb8bb5f3e4940a12d7d0e
zephyron1237 <princessbowl@gmail.com>
2021-08-21 08:01:15 -0700

    All Industries popup Trade tab cps fix
    In the Trade tab of the All Industries popup, the total CPS at the top was not updating.  This fixes that by fixing the last selector in updateTradeTabTotals that was not specifically targeting shown modals.

--- missions.js
+++ missions.js
@@ -2776,7 +2776,7 @@ function updateTradeTabTotals(researcher = null) {
   saveFormValues(formValues);
   
   // Fill in values in the trade tab.
-  $('#totalDerivedComrades').text(bigNum(formValues.Trades.TotalComrades));
+  $('.modal.show #totalDerivedComrades').text(bigNum(formValues.Trades.TotalComrades));
   
   let industryTrades = getIndustryTradeBreakdown(formValues);
   for (let industryTrade of industryTrades) {
